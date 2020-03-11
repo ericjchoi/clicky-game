2265ea2d55957932a125ede1fcde80ddee929f03
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2020-03-10 20:31:40 -0700

    code cleaned /src/components/onePictureContainer.js

--- src/components/onePictureContainer.js
+++ src/components/onePictureContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./onePictureContainer.css";
 
+// For individual picture
 function OnePictureContainer(props) {
     return (
         <img onClick={() => props.stateHandle(props.name)} src={props.url} alt={props.name} className="img-thumbnail" style={{ width: 10 + "em" }} />
     );
 }
 
+// Export OnePictureContainer component
 export default OnePictureContainer;
